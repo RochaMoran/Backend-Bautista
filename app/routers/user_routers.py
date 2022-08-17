067e9f402a290903db8015035b1b98ebde83ad6a
RochaMoran <rochamoran1@gmail.com>
2022-08-16 21:28:25 -0600

    eliminando importaciones no utilizadas

--- app/routers/user_routers.py
+++ app/routers/user_routers.py
@@ -1,70 +1,73 @@
-from urllib import response
-from fastapi import APIRouter, HTTPException, Header, Path, Depends, status
-from requests import session
-from sqlalchemy import null
+from fastapi import APIRouter, Header, Depends
 from sqlalchemy.orm import Session
 
 from app.db.config import SessionLocal
-from app.schemas.user_schemas import UserSchema, RequestUser, UserResponse, RequestLogin
+from app.schemas.user_schemas import RequestUser, UserResponse, RequestLogin
 from app.db import user
 from app.utils.auth import Encrypter
 from app.utils import auth
 
 router = APIRouter()
 
+
 def get_db():
     db = SessionLocal()
-    try: 
+    try:
         yield db
     finally:
         db.close()
-    
+
+
 @router.get("/")
-async def get(db:Session=Depends(get_db), Authorization: str = Header(None)):
+async def get(db: Session = Depends(get_db), Authorization: str = Header(None)):
     try:
         token = Authorization.split(" ")[1]
         isValid = auth.validate_token(token=token, output=True)
         if isValid == "Invalid token":
-            return UserResponse(code=401, status="fail",message="Invalid token")
+            return UserResponse(code=401, status="fail", message="Invalid token")
 
-        _users = user.get_user(db,0,100)
-        return UserResponse(code=200, status="ok",message="Success fetch all data", result=_users)
+        _users = user.get_user(db, 0, 100)
+        return UserResponse(code=200, status="ok", message="Success fetch all data", result=_users)
     except:
-         return UserResponse(code=401, status="fail",message="Invalid token")
+        return UserResponse(code=401, status="fail", message="Invalid token")
+
 
 @router.get("/current")
-async def get_current_user(db:Session=Depends(get_db), Authorization: str = Header(None)):
+async def get_current_user(db: Session = Depends(get_db), Authorization: str = Header(None)):
     try:
         token = Authorization.split(" ")[1]
         isValid = auth.validate_token(token=token, output=True)
         if isValid == "Invalid token":
-            return UserResponse(code=401, status="fail",message="Invalid token")
-        
+            return UserResponse(code=401, status="fail", message="Invalid token")
+
         _user = user.get_user_by_email(db, isValid)
         _user.__dict__['user_password'] = ""
-        return UserResponse(code=200, status="ok",message="Success fetch current user", result=_user)
+        return UserResponse(code=200, status="ok", message="Success fetch current user", result=_user)
     except:
-         return UserResponse(code=401, status="fail",message="Invalid token")
+        return UserResponse(code=401, status="fail", message="Invalid token")
+
 
 @router.post('/register')
-async def create(request:RequestUser,db:Session=Depends(get_db)):
+async def create(request: RequestUser, db: Session = Depends(get_db)):
 
     exist_user = user.get_user_by_email(db, request.parameter.user_email)
     if exist_user != None:
-        return UserResponse(code=200, status="ok",message="User already exist")
+        return UserResponse(code=200, status="ok", message="User already exist")
 
-    password_hashed= Encrypter.encript( request.parameter.user_password)
-    
+    password_hashed = Encrypter.encript(request.parameter.user_password)
+
     user.create_user(db, request.parameter, password_hashed=password_hashed)
-    token : str = auth.generate_jwt({"user_name" : request.parameter.user_name})
+    token: str = auth.generate_jwt({"user_name": request.parameter.user_name})
     return UserResponse(code=200, status="ok", message="User created successfully", result=token).dict(exclude_none=True)
-    
+
+
 @router.post('/login')
-async def login_user(request: RequestLogin, db:Session=Depends(get_db)):
+async def login_user(request: RequestLogin, db: Session = Depends(get_db)):
     try:
-        _user = user.get_user_login(db, request.parameter.user_email, request.parameter.user_password)
+        _user = user.get_user_login(
+            db, request.parameter.user_email, request.parameter.user_password)
     except:
-         return UserResponse(
+        return UserResponse(
             code=404,
             status="Fail",
             message="Emial or Password is wrong"
@@ -77,23 +80,24 @@ async def login_user(request: RequestLogin, db:Session=Depends(get_db)):
             message="User not found",
             result=_user
         )
-    
-    token : str = auth.generate_jwt({"user_name" : request.parameter.user_email})
+
+    token: str = auth.generate_jwt({"user_name": request.parameter.user_email})
     return UserResponse(code=200, status="ok", message="Login successfully", result=token).dict(exclude_none=True)
 
+
 @router.post('/update/{user_id}')
-async def update_user(request: RequestUser, user_id, db:Session=Depends(get_db), Authorization: str = Header(None)):
+async def update_user(request: RequestUser, user_id, db: Session = Depends(get_db), Authorization: str = Header(None)):
     try:
         token = Authorization.split(" ")[1]
         isValid = auth.validate_token(token=token, output=True)
         if isValid == "Invalid token":
-            return UserResponse(code=401, status="fail",message="Invalid token")
+            return UserResponse(code=401, status="fail", message="Invalid token")
 
         intId = int(user_id)
 
         exist_user = user.get_user_by_email(db, request.parameter.user_email)
         if exist_user != None:
-            return UserResponse(code=200, status="ok",message="Another user already has that email")
+            return UserResponse(code=200, status="ok", message="Another user already has that email")
 
         _user = user.update_user(db, request, intId)
 
@@ -104,25 +108,25 @@ async def update_user(request: RequestUser, user_id, db:Session=Depends(get_db),
                 message="User not found",
                 result=_user
             )
-        
+
         return UserResponse(code=200, status="ok", message="Update successfully")
     except:
-         return UserResponse(code=401, status="fail",message="Invalid token")
-    
+        return UserResponse(code=401, status="fail", message="Invalid token")
+
 
 @router.delete('/delete/{user_id}')
-async def update_user(user_id, db:Session=Depends(get_db), Authorization: str = Header(None)):
+async def update_user(user_id, db: Session = Depends(get_db), Authorization: str = Header(None)):
     try:
         token = Authorization.split(" ")[1]
         isValid = auth.validate_token(token=token, output=True)
         if isValid == "Invalid token":
-            return UserResponse(code=401, status="fail",message="Invalid token")
-        
+            return UserResponse(code=401, status="fail", message="Invalid token")
+
         exist_user = user.get_user_by_id(db=db, user_id=user_id)
         if exist_user == None:
-            return UserResponse(code=200, status="ok",message="the user does not exist")
+            return UserResponse(code=200, status="ok", message="the user does not exist")
 
         user.remove_user(db=db, user_id=user_id)
-        return UserResponse(code=200, status="ok",message="success delete user")
+        return UserResponse(code=200, status="ok", message="success delete user")
     except:
-         return UserResponse(code=401, status="fail",message="Invalid token")
+        return UserResponse(code=401, status="fail", message="Invalid token")
--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,41 +1,46 @@
-from datetime import datetime
-from optparse import Option
-from typing import List, Optional, Generic, TypeVar
+from typing import Optional, Generic, TypeVar
 from pydantic import BaseModel, Field
 from pydantic.generics import GenericModel
 
 T = TypeVar("T")
 
+
 class RolesSchema(BaseModel):
-    roles_id: Optional[int]=None
-    roles_name: Optional[str]=None
+    roles_id: Optional[int] = None
+    roles_name: Optional[str] = None
 
     class Config:
         orm_mode = True
+
 
 class UserSchema(BaseModel):
-    user_name: Optional[str]=None
-    user_email: Optional[str]=None
-    user_password: Optional[str]=None
-    user_id_rol: Optional[int]= Field(default=None, foreign_key="roles.roles_id")
-    user_status: Optional[str]=None
+    user_name: Optional[str] = None
+    user_email: Optional[str] = None
+    user_password: Optional[str] = None
+    user_id_rol: Optional[int] = Field(
+        default=None, foreign_key="roles.roles_id")
+    user_status: Optional[str] = None
 
     class Config:
         orm_mode = True
+
 
 class RequestUser(BaseModel):
     parameter: UserSchema = Field(...)
+
 
 class UserResponse(GenericModel, Generic[T]):
     code: str
     status: str
     message: str
     result: Optional[T]
-    
+
+
 class LoginModel(BaseModel):
-    user_email: Optional[str]=None
-    user_password: Optional[str]=None
+    user_email: Optional[str] = None
+    user_password: Optional[str] = None
     pass
+
 
 class RequestLogin(BaseModel):
     parameter: LoginModel = Field(...)
